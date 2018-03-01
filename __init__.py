3fc00ec3b7cfc79fa499378de53defbc00e3a540
theJonan <ivan@jonan.info>
2018-03-01 23:24:08 +0200

    Fixes on submodules inclusion

--- __init__.py
+++ __init__.py
@@ -1,3 +1,14 @@
+from .dt_utils import *
+from .dt_constants import *
+from .nn_utils import  *
+from .tf_hooks import *
+from .tf_persist import *
+from .tf_utils import *
+
+# This needs to stay before the actual model import directives
 TF_MODELS = dict()
+
+from .tf_vae import *
+from .tf_ae import *
 
 __all__ = ["dt_constants", "dt_utils", "nn_utils", "tf_utils", "tf_hooks", "tf_persist", "tf_ae", "tf_vae", "TF_MODELS"]
